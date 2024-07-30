e82f27abdf15a20cbd41c4126db3415f4a54fdd6
marco <marco@examp.com>
2024-07-30 13:57:09 -0500

    Co-authored-by: DavidMejia <DavidMejia2369@users.noreply.github.com>

--- detalles_compra/models.py
+++ detalles_compra/models.py
@@ -3,7 +3,9 @@ from django.apps import apps
 
 class DetalleOrdenCompra(models.Model):
     orden = models.ForeignKey('ordenes_compras.OrdenCompra', on_delete=models.CASCADE)
-    articles = models.ForeignKey('articles.Article', on_delete=models.CASCADE)
+    producto = models.ForeignKey('articles.Article', on_delete=models.CASCADE)
     cantidad = models.IntegerField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     
+    def __str__(self):
+        return self.producto
