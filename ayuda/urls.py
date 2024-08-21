c7118973e1a17c8f1ef16e8b95a42c0aea49f771
Marco A Mesa Caceres <marco_mesa@soy.sena.edu.co>
2024-08-21 15:36:23 -0500

    commit 21082024 16 horas

--- ayuda/urls.py
+++ ayuda/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.lista_ayuda, name='lista_ayuda'),
     path('<int:pk>/', views.detalle_ayuda, name='detalle_ayuda'),
-    path('manual_usuario/', views.manual_usuario, name='manual_usuario'),
+    path('manual-usuario/', views.ManualUsuarioView.as_view(), name='manual_usuario'),
     path('descargar-manual/', views.descargar_manual, name='descargar_manual'),
 ]
--- ayuda/views.py
+++ ayuda/views.py
@@ -1,7 +1,8 @@
+from django.views.generic.base import TemplateView
 from django.shortcuts import render, get_object_or_404
 from django.conf import settings
 from .models import ArticuloAyuda
-from django.http import FileResponse
+from django.http import FileResponse, HttpResponse
 import os
 
 def lista_ayuda(request):
@@ -19,3 +20,20 @@ def manual_usuario(request):
 def descargar_manual(request):
     pdf_path = os.path.join(settings.STATICFILES_DIRS[0], 'docs/Manual_de_Usuario_Ardecors.pdf')
     return FileResponse(open(pdf_path, 'rb'), as_attachment=True, content_type='application/pdf')
+
+class ManualUsuarioView(TemplateView):
+    template_name = 'ayuda/manual_usuario.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pdf_path = os.path.join(settings.STATICFILES_DIRS[0], 'docs/Manual_de_Usuario_Ardecors.pdf')
+        context['pdf_url'] = f'{settings.STATIC_URL}docs/Manual_de_Usuario_Ardecors.pdf'
+        return context
+    
+def descargar_manual(request):
+    pdf_path = 'D:\\ardecors_django\\sistema\\ayuda\\static\\docs\\Manual_de_Usuario_Ardecors.pdf'
+    if os.path.exists(pdf_path):
+        return FileResponse(open(pdf_path, 'rb'), as_attachment=True, content_type='application/pdf')
+    else:
+        # Handle the case where the file is not found
+        return HttpResponse("The file could not be found.")    
