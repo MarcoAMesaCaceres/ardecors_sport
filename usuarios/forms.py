5e865c40fb1eb5f2b7f28598df5d2eb24e952c94
Marco A Mesa Caceres <marco_mesa@soy.sena.edu.co>
2024-08-22 15:22:37 -0500

    commit 22082024 15 horas

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -3,11 +3,14 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, Pass
 from django.contrib.auth.models import User
 
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Usuario'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Contraseña'}))
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Usuario'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Contraseña'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirmar Contraseña'}))
 
     class Meta:
         model = User
--- usuarios/static/js/usuarios.js
+++ usuarios/static/js/usuarios.js
@@ -1,22 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const rows = document.querySelectorAll('tbody tr');
+const container = document.getElementById('container');
+const registerBtn = document.querySelector('.toggle-right .btn');
+const loginBtn = document.querySelector('.toggle-left .btn');
 
-    rows.forEach(row => {
-        row.addEventListener('click', function() {
-            const userId = this.querySelector('td').textContent;
-            alert(`Usuario ID: ${userId}`);
-            // Acciones adicionales al hacer clic en una fila
-        });
+if (registerBtn) {
+    registerBtn.addEventListener('click', () => {
+        container.classList.add("active");
     });
+}
 
-    const newUserButton = document.querySelector('.btn');
-    if (newUserButton) {
-        newUserButton.addEventListener('click', function(event) {
-            event.preventDefault();
-            window.location.href = this.href;
-        });
-    }
-
-    console.log("Usuarios cargados.");
-});
-
+if (loginBtn) {
+    loginBtn.addEventListener('click', () => {
+        container.classList.remove("active");
+    });
+}
