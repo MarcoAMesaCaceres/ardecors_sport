30b4aacb0d5c15099783aa0bdc20a9c8f9aa2d6c
Marco A Mesa Caceres <marco_mesa@soy.sena.edu.co>
2024-09-09 15:34:08 -0500

    commit 09092024 15:30 horas

--- articles/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Article  # Cambia de articles a Article
-
-# Registra tus modelos aquí
-admin.site.register(Article)
--- proveedores/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('proveedores/', views.lista_proveedores, name='lista_proveedores'),
-    path('crear/', views.crear_proveedor, name='crear_proveedor'),
-    path('editar/<int:pk>/', views.editar_proveedor, name='editar_proveedor'),
-    path('<int:pk>/eliminar/', views.eliminar_proveedor, name='eliminar_proveedor'),
-]
