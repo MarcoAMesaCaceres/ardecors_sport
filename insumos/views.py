029946d3ab718649d32b036ab46832067ab9753d
Marco A Mesa Caceres <marco_mesa@soy.sena.edu.co>
2024-10-18 15:27:58 -0500

    commit filtros de insumos

--- insumos/views.py
+++ insumos/views.py
@@ -7,7 +7,42 @@ from django.core.exceptions import ValidationError
 from proveedores.models import  Proveedor
 
 def lista_insumos(request):
+    id_query = request.GET.get('id', '')
+    nombre_query = request.GET.get('nombre', '').strip()
+    descripcion_query = request.GET.get('descripcion', '').strip()
+    proveedor_query = request.GET.get('proveedor', '').strip()
+    precio_query = request.GET.get('precio', '').strip()
+    stock_query = request.GET.get('stock', '').strip()
+
+    # Validación del ID
+    if id_query and not id_query.isdigit():
+        messages.error(request, "Debe colocar un número válido para el ID.")
+        return render(request, 'lista_insumos.html', {'insumos': Insumos.objects.none()})
+
     insumos = Insumos.objects.all()
+
+    if id_query:
+        insumos = insumos.filter(id=id_query)
+    if nombre_query:
+        insumos = insumos.filter(nombre__icontains=nombre_query)
+    if descripcion_query:
+        insumos = insumos.filter(descripcion__icontains=descripcion_query)
+    if proveedor_query:
+        # Asegúrate de que 'nombre' sea el campo correcto del modelo Proveedor
+        insumos = insumos.filter(proveedor__nombre__icontains=proveedor_query)
+    if precio_query:
+        try:
+            precio_value = float(precio_query)
+            insumos = insumos.filter(precio=precio_value)
+        except ValueError:
+            messages.error(request, "El precio debe ser un número válido.")
+    if stock_query:
+        try:
+            stock_value = int(stock_query)
+            insumos = insumos.filter(stock=stock_value)
+        except ValueError:
+            messages.error(request, "El stock debe ser un número válido.")
+
     return render(request, 'lista_insumos.html', {'insumos': insumos})
 
 
